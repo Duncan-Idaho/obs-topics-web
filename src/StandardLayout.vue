7e15477a9d912dbefed84e01385571e3cece2a3a
Edouard Paumier <epaumier@kds.com>
2023-10-16 00:02:32 +0200

    Making the background transparent for non standard layout

--- src/StandardLayout.vue
+++ src/StandardLayout.vue
@@ -4,22 +4,46 @@ import PreviewTooltip from '@/components/PreviewTooltip.vue'
 </script>
 
 <template>
-  <header>
-    <PreviewTooltip/>
-    <nav>
-      <RouterLink to="/import">Import</RouterLink>
-      <RouterLink to="/">Topics</RouterLink>
-      <RouterLink to="/columns">Columns displayed</RouterLink>
-      <RouterLink to="/settings">Settings</RouterLink>
-    </nav>
-  </header>
+  <div class="standard-layout">
+    <header>
+      <PreviewTooltip/>
+      <nav>
+        <RouterLink to="/import">Import</RouterLink>
+        <RouterLink to="/">Topics</RouterLink>
+        <RouterLink to="/columns">Columns displayed</RouterLink>
+        <RouterLink to="/settings">Settings</RouterLink>
+      </nav>
+    </header>
 
-  <main>
-    <RouterView />
-  </main>
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <style scoped>
+.standard-layout {
+  display: flex;
+  flex-flow: column;
+  height: 100vh;
+  background: var(--color-background);
+  padding-left: 0.4rem;
+  padding-right: 0.4rem;
+}
+
+@media (min-width: 800px) {
+  .standard-layout {
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+}
+@media (min-height: 800px) {
+  .standard-layout {
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+  }
+}
+
 header {
   line-height: 1.5;
   display: flex;
